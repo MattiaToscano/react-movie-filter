bbfe5160a1228131a87d603963048354dbf75134
MattiaToscano <mattia1997ct@gmail.com>
2025-05-12 13:31:37 +0200

    [UPD] Inserita lista contenente array di film

--- react-movie-filter/src/App.jsx
+++ react-movie-filter/src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
+import React from 'react'
+import { useState, useEffect } from 'react'
 
+const films = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
 
-
-function App() {
+const App = () => {
 
   return (
-    <>
-
-    </>
+    <div className="container">
+      <h1 className="lista-film">Movie Filter</h1>
+      <ul className="list-group">
+        {films.map((film, index) => (
+          <li key={film.id} className="list-group-item">
+            <h2>{film.title}</h2>
+            <p>{film.genre}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
