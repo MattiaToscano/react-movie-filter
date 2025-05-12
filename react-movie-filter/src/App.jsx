1dcff145f1628246702beeba1c526b16f9e342ad
MattiaToscano <mattia1997ct@gmail.com>
2025-05-12 13:37:50 +0200

    [UPD] Inserito select

--- react-movie-filter/src/App.jsx
+++ react-movie-filter/src/App.jsx
@@ -11,12 +11,17 @@ const films = [
 ]
 
 const App = () => {
-
+  const [search, setSearch] = useState('');
   return (
     <div className="container">
       <h1 className="lista-film">Movie Filter</h1>
+      <select className="form-select" onChange={(e) => setSearch(e.target.value)}>
+        {films.map((film) => (
+          <option key={film.title} value={film.genre}>{film.genre}</option>
+        ))}
+      </select>
       <ul className="list-group">
-        {films.map((film, index) => (
+        {films.map((film) => (
           <li key={film.id} className="list-group-item">
             <h2>{film.title}</h2>
             <p>{film.genre}</p>
